1a5aef8ff0aa612e622c656d4bdef6c84a28741c
Venkat <venkatesan255@gmail.com>
2025-10-05 13:51:20 +0100

    added support for analysis template in test content

---
+++ src/main/java/com/lre/model/test/testcontent/analysistemplate/AnalysisTemplate.java
@@ -0,0 +1,18 @@
+package com.lre.model.test.testcontent.analysistemplate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static com.lre.actions.helpers.ConfigConstants.LRE_API_XMLNS;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnalysisTemplate {
+    @JsonProperty("ID")
+    @JacksonXmlProperty(localName = "ID", namespace = LRE_API_XMLNS)
+    private Integer id;
+}
