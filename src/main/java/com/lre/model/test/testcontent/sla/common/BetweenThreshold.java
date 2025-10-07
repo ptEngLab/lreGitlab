05d9138914a6a27e9617f154b7fd80a99beb056e
Venkat <venkatesan255@gmail.com>
2025-10-07 16:24:16 +0100

    fix for BetweenThreshold tag

--- src/main/java/com/lre/model/test/testcontent/sla/common/BetweenThreshold.java
+++ src/main/java/com/lre/model/test/testcontent/sla/common/BetweenThreshold.java
@@ -1,6 +1,7 @@
 package com.lre.model.test.testcontent.sla.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class BetweenThreshold {
 
     @JsonProperty("Threshold")
     @JacksonXmlProperty(localName = "Threshold", namespace = LRE_API_XMLNS)
+    @JacksonXmlElementWrapper(useWrapping = false)
     private List<Float> threshold;
 
 }
